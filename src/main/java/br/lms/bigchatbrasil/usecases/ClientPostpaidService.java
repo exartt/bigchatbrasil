281524a2710125a7484967f947dcb13e034b995e
Exartt <lmoraes1644cadastros@gmail.com>
2024-03-03 11:23:42 -0300

    add logs.

--- src/main/java/br/lms/bigchatbrasil/usecases/ClientPostpaidService.java
+++ src/main/java/br/lms/bigchatbrasil/usecases/ClientPostpaidService.java
@@ -8,6 +8,8 @@ import br.lms.bigchatbrasil.domain.enums.PlanType;
 import br.lms.bigchatbrasil.domain.model.Client;
 import br.lms.bigchatbrasil.domain.service.IClientPostpaidService;
 import br.lms.bigchatbrasil.domain.service.IClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
@@ -19,6 +21,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ClientPostpaidService implements IClientPostpaidService {
+    private static final Logger logger = LoggerFactory.getLogger(ClientPostpaidService.class);
     private final IClientService clientService;
     private final BigDecimal MESSAGE_FEE;
     @Autowired
@@ -32,9 +35,16 @@ public class ClientPostpaidService implements IClientPostpaidService {
     @Override
     @Transactional
     public CompletableFuture<Void> updateLimitAmount(long clientId, PostpaidDTO postpaidDTO) {
-        Client client = clientService.getClientReferenceById(clientId);
-        validateClientForPostpaidUpdate(client, postpaidDTO);
-        updateClientPostpaidData(client, postpaidDTO);
+        logger.info("Starting updateLimitAmount for client ID: {}", clientId);
+        try {
+            Client client = clientService.getClientReferenceById(clientId);
+            validateClientForPostpaidUpdate(client, postpaidDTO);
+            updateClientPostpaidData(client, postpaidDTO);
+            logger.info("Completed updateLimitAmount for client ID: {}", clientId);
+        } catch (Exception e) {
+            logger.error("Error in updateLimitAmount for client ID: {}: {}", clientId, e.getMessage(), e);
+            throw e;
+        }
         return CompletableFuture.completedFuture(null);
     }
     @Override
@@ -45,7 +55,9 @@ public class ClientPostpaidService implements IClientPostpaidService {
     }
 
     private void validateClientForPostpaidUpdate(Client client, PostpaidDTO postpaidDTO) {
+        logger.debug("Validating client for postpaid update: {}", client.getId());
         if (client.getPlanType() != PlanType.POSTPAID) {
+            logger.error("Invalid plan type for client ID: {}", client.getId());
             throw new InvalidPlanTypeException(PlanType.POSTPAID.getPlanName());
         }
 
@@ -53,15 +65,20 @@ public class ClientPostpaidService implements IClientPostpaidService {
         float spentAmount = client.getClientPostpaid().getSpentValue();
 
         if (newAmountLimit < spentAmount) {
+            logger.error("New amount limit {} is less than the spent amount {} for client ID: {}", newAmountLimit, spentAmount, client.getId());
             throw new LimitValueExceededException(newAmountLimit, spentAmount);
         }
 
         if (newAmountLimit <= 0) {
+            logger.error("Invalid limit value {} for client ID: {}", newAmountLimit, client.getId());
             throw new InvalidLimitValueException();
         }
+
+        logger.debug("Client validation for postpaid update passed for client ID: {}", client.getId());
     }
 
     private void updateClientPostpaidData(Client client, PostpaidDTO postpaidDTO) {
+        logger.debug("Updating postpaid data for client ID: {}", client.getId());
         client.getClientPostpaid().setLimitValue(postpaidDTO.getLimitValue());
     }
 }
